2b6176788bbcda81b45425fc5b464a833377fff3
Wolfram Diestel <wolfram.diestel@t-systems.com>
2022-06-16 17:25:03 +0200

    navigebla graphml-svg

---
+++ pro/trf/graphml2model.pl
@@ -0,0 +1,27 @@
+:- use_module(library(sgml)).
+:- use_module(library(xpath)).
+:- use_module(library(http/json)).
+
+input('cikloj/fosforciklo-detala.graphml').
+output('cikloj/fosforciklo-detala.mdl').
+
+go :-
+    input(File),
+    load_xml(File,DOM,[]),
+    edges(DOM,JSON),
+    % skribu rezultojn
+    output(OutFile),
+    open(OutFile,write,Output),
+    json_write(Output,JSON),
+    close(Output).
+
+edges(DOM,JSON) :-
+    findall(Edge,xpath(DOM,//edge,Edge),Edges),
+    foldl(edge,Edges,_{},JSON),!.
+
+edge(Edge,JIn,JOut) :-
+    Edge = element(edge,Attr,_Cnt),
+    member(id=Id,Attr),
+    member(source=Src,Attr),
+    member(target=Trg,Attr),
+    JOut = JIn.put(Id,[Src,Trg]).
