f1489465b5949807db37d6407f5d9154b31b11e5
Wolfram Diestel <wolfram.diestel@t-systems.com>
2022-06-23 17:29:52 +0200

    grafiko: fosfor- k sulfuciklo

--- pro/trf/graphml2model.pl
+++ pro/trf/graphml2model.pl
@@ -2,18 +2,22 @@
 :- use_module(library(xpath)).
 :- use_module(library(http/json)).
 
-input('cikloj/fosforciklo-detala.graphml').
-output('cikloj/fosforciklo-detala.mdl').
+graph('cikloj/fosforciklo-detala').
+graph('cikloj/sulfurciklo-detala').
+%output('cikloj/fosforciklo-detala.mdl').
 
 go :-
-    input(File),
-    load_xml(File,DOM,[]),
-    edges(DOM,JSON),
-    % skribu rezultojn
-    output(OutFile),
-    open(OutFile,write,Output),
-    json_write(Output,JSON),
-    close(Output).
+    forall(graph(G),
+    (
+        atom_concat(G,'.graphml',File),
+        load_xml(File,DOM,[]),
+        edges(DOM,JSON),
+        % skribu rezultojn
+        atom_concat(G,'.mdl',OutFile),
+        open(OutFile,write,Output),
+        json_write(Output,JSON),
+        close(Output)
+    )).
 
 edges(DOM,JSON) :-
     findall(Edge,xpath(DOM,//edge,Edge),Edges),
