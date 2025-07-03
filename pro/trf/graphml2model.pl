1f290ffae49c5b4210716afcb8148142f3c77719
Wolfram Diestel <diestel@steloj.de>
2025-07-03 21:02:33 +0200

    reordigo de modeldosieroj

--- pro/trf/graphml2model.pl
+++ pro/trf/graphml2model.pl
@@ -2,8 +2,21 @@
 :- use_module(library(xpath)).
 :- use_module(library(http/json)).
 
-graph('cikloj/fosforciklo-detala').
-graph('cikloj/sulfurciklo-detala').
+/**
+ * extrakti la nodojn kaj eĝojn inter nodoj
+ * el graphml-dosiero (kreita de yEd)
+ * 
+ * Ni bezonas tiujn rilatojn, ĉar ili en sVG-eksporto malestas,
+ * sed ni volas uzi la modelon per Javoskripto en la paĝoj
+ * por ŝanĝi la staton de la model, ekz- transiri de unu
+ * aktiva nodo al alia ks.
+ */
+
+% graph('modeloj/elementcikloj/fosforciklo-detala').
+% graph('modeloj/elementcikloj/sulfurciklo-detala').
+
+graph('modeloj/procezoj/TCA_SGBN_PD').
+
 %output('cikloj/fosforciklo-detala.mdl').
 
 go :-
@@ -11,13 +24,40 @@ go :-
     (
         atom_concat(G,'.graphml',File),
         load_xml(File,DOM,[]),
-        edges(DOM,JSON),
+        nodes(DOM,JSON1),
+        edges(DOM,JSON2),
+        % put_dict(JSON1,JSON2,JSON),
         % skribu rezultojn
         atom_concat(G,'.mdl',OutFile),
         open(OutFile,write,Output),
-        json_write(Output,JSON),
+        json_write(Output,_{nodoj: JSON1, eĝoj: JSON2}),
         close(Output)
     )).
+
+/*
+    <node id="n1" yfiles.foldertype="group">
+    <data key="d6">
+      <y:ProxyAutoBoundsNode>
+        <y:Realizers active="0">
+          <y:GroupNode>
+            <y:Geometry height="898.1713321493626" width="967.981943111052" x="260.7179767432283" y="-232.96875"/>
+            <y:Fill hasColor="false" transparent="false"/>
+            <y:BorderStyle color="#000000" type="line" width="3.0"/>
+            <y:NodeLabel alignment="center" autoSizePolicy="content" borderDistance="0.0" fontFamily="Dialog" fontSize="12" fontStyle="plain" hasBackgroundColor="false" hasLineColor="false" height="17.96875" horizontalTextPosition="center" iconTextGap="4" modelName="internal" modelPosition="t" textColor="#000000" verticalTextPosition="bottom" visible="true" width="76.849609375" x="445.566166868026" xml:space="preserve" y="0.0">mitoĥondrio</y:NodeLabel>
+*/
+
+nodes(DOM,JSON) :-
+    findall(Node,xpath(DOM,//node,Node),Nodes),
+    foldl(node,Nodes,_{},JSON),!.
+
+node(Node,JIn,JOut) :-
+    Node = element(node,Attr,_Cnt),
+    member(id=Id,Attr),
+    %member(yfiles.foldertype=Type,Attr),
+    % print(Node),
+    xpath(Node,//'y:NodeLabel'(normalize_space),Label),
+    %print(Label),
+    JOut = JIn.put(Id,Label).
 
 edges(DOM,JSON) :-
     findall(Edge,xpath(DOM,//edge,Edge),Edges),
